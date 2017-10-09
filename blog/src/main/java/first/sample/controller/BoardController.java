78ab32d01f79eedf71f28ab26e8435eded587398
imt30 <imt30@DESKTOP-I6QGV4D>
2017-10-10 00:03:17 +0900

    controller

---
+++ blog/src/main/java/first/sample/controller/BoardController.java
@@ -0,0 +1,30 @@
+package first.sample.controller;
+
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import first.sample.service.BoardService;
+
+@Controller
+public class BoardController {
+	Logger log = Logger.getLogger(this.getClass());
+	
+	@Resource(name="BoardService")
+	private BoardService boardService;
+	
+	@RequestMapping(value="/board/boardView.do")
+    public ModelAndView openSampleBoardList(Map<String,Object> commandMap) throws Exception{
+    	ModelAndView mv = new ModelAndView("/boardView");
+    	
+//    	List<Map<String,Object>> list = boardService.selectBoardList(commandMap);
+//    	mv.addObject("list", list);
+    	
+    	return mv;
+    }
+}
--- blog/src/main/java/first/sample/controller/CommonController.java
+++ blog/src/main/java/first/sample/controller/CommonController.java
@@ -10,22 +10,78 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import first.sample.service.SampleService;
+import first.sample.service.CommonService;
 
 @Controller
-public class SampleController {
+public class CommonController {
 	Logger log = Logger.getLogger(this.getClass());
 	
-	@Resource(name="sampleService")
-	private SampleService sampleService;
-	
-	@RequestMapping(value="/sample/openSampleBoardList.do")
+	@Resource(name="CommonService")
+	private CommonService commonService;
+
+	@RequestMapping(value="/common/openSampleBoardList.do")
     public ModelAndView openSampleBoardList(Map<String,Object> commandMap) throws Exception{
+		System.out.println("/common/openSampleBoardList.do");
     	ModelAndView mv = new ModelAndView("/sample/boardList");
     	
-    	List<Map<String,Object>> list = sampleService.selectBoardList(commandMap);
+    	List<Map<String,Object>> list = commonService.selectBoardList(commandMap);
     	mv.addObject("list", list);
     	
     	return mv;
     }
+
+	@RequestMapping(value="/common/main.do")
+    public ModelAndView main(Map<String,Object> commandMap) throws Exception{
+		System.out.println("/common/openSampleBoardList.do");
+    	ModelAndView mv = new ModelAndView("/src/main");
+    	
+    	List<Map<String,Object>> list = commonService.selectBoardList(commandMap);
+    	mv.addObject("list", list);
+    	
+    	return mv;
+    }
+	
+	@RequestMapping(value="/common/profile.do")
+	public ModelAndView profile(Map<String,Object> commandMap) throws Exception{
+		System.out.println("/common/openSampleBoardList.do");
+		ModelAndView mv = new ModelAndView("/profile/profile");
+		
+		List<Map<String,Object>> list = commonService.selectBoardList(commandMap);
+		mv.addObject("list", list);
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="/common/contacts.do")
+	public ModelAndView contacts(Map<String,Object> commandMap) throws Exception{
+		System.out.println("/common/openSampleBoardList.do");
+		ModelAndView mv = new ModelAndView("/profile/contacts");
+		
+		List<Map<String,Object>> list = commonService.selectBoardList(commandMap);
+		mv.addObject("list", list);
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="/common/mailbox.do")
+	public ModelAndView mailbox(Map<String,Object> commandMap) throws Exception{
+		System.out.println("/common/openSampleBoardList.do");
+		ModelAndView mv = new ModelAndView("/profile/mailbox");
+		
+		List<Map<String,Object>> list = commonService.selectBoardList(commandMap);
+		mv.addObject("list", list);
+		
+		return mv;
+	}
+	
+	@RequestMapping(value="/common/login.do")
+	public ModelAndView login(Map<String,Object> commandMap) throws Exception{
+		System.out.println("/common/openSampleBoardList.do");
+		ModelAndView mv = new ModelAndView("/profile/login");
+		
+		List<Map<String,Object>> list = commonService.selectBoardList(commandMap);
+		mv.addObject("list", list);
+		
+		return mv;
+	}
 }
