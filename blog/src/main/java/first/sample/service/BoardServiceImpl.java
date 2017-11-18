e3e39be0c78ad67ee1d5c59c9d4652452b3b3664
imt30 <imt30@DESKTOP-I6QGV4D>
2017-11-18 21:06:06 +0900

    test

--- blog/src/main/java/first/sample/service/BoardServiceImpl.java
+++ blog/src/main/java/first/sample/service/BoardServiceImpl.java
@@ -8,18 +8,19 @@ import javax.annotation.Resource;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
-import first.sample.dao.DAO;
+import first.sample.dao.CommonDao;
 
 @Service("BoardService")
 public class BoardServiceImpl implements BoardService{
 	Logger log = Logger.getLogger(this.getClass());
 	
-	@Resource(name="DAO")
-	private DAO dao;
+	@Resource(name="CommonDao")
+	private CommonDao dao;
 	
 	@Override
 	public List<Map<String, Object>> selectBoardList(Map<String, Object> map) throws Exception {
-		return dao.selectBoardList(map);
+//		return dao.selectBoardList(map);
+		return null;
 		
 	}
 
